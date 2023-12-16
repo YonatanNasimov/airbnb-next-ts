c72df953e9f7fdc952ee6426e5eca3ad3504736f
YonatanNasimov <yonatannasimov@gmail.com>
2023-12-16 20:34:07 +0200

    properties

--- src/components/navbar/userMenu.tsx
+++ src/components/navbar/userMenu.tsx
@@ -98,7 +98,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
                 <MenuItem label="My trips" onClick={() => router.push("/trips")} />
                 <MenuItem label="My favorites" onClick={() => router.push("/favorites")} />
                 <MenuItem label="My reservations" onClick={() => router.push("/reservations")} />
-                <MenuItem label="My properties" onClick={() => { }} />
+                <MenuItem label="My properties" onClick={() => router.push("/properties")} />
                 <MenuItem label="Airbnb your home" onClick={onRent} />
                 <hr />
                 <MenuItem label="Logout" onClick={() => signOut()} />
